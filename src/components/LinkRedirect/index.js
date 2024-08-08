882d94c79d597de6d6946f2f830cacee5835d265
Abeeb Abiola Adeniyi <abiolayoung229@gmail.com>
2024-08-08 22:29:24 +0300

    adjustment

--- src/components/LinkRedirect/index.js
+++ src/components/LinkRedirect/index.js
@@ -1,8 +1,9 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
-import { getFirestore, doc, getDoc } from "firebase/firestore";
+import { getFirestore, doc, getDoc, updateDoc, increment } from "firebase/firestore";
 import { CircularProgress, Box, Typography } from "@mui/material";
+import { app, firestore } from "../../firebase";
 
 const LinkRedirect = () => {
   const [loading, setLoading] = useState(true);
@@ -13,12 +14,26 @@ const LinkRedirect = () => {
     const fetchLinkDoc = async () => {
       const linkDocRef = doc(db, "links", shortCode);
       const linkDocSnap = await getDoc(linkDocRef);
-      if (linkDocSnap.exists) {
-        const { longUrl } = linkDocSnap.data();
+    //   console.log(linkDocSnap.data());
+    //   if (linkDocSnap.exists) {
+    //     const { longUrl, linkID, userUid } = linkDocSnap.data();
+    //     doc(
+    //         collection(getFirestore(app), "users", userUid, "links", linkID)).update({
+    //         totalClicks: app.firestore.FieldValue.increment(1)
+    //       });
+    //     window.location.href = longUrl;
+    //   } else {
+    //     setLoading(false);
+    //   }
+
+    if (linkDocSnap.exists) {
+        const { longUrl, linkID, userUid } = linkDocSnap.data();
+        const linkDocRef = doc(getFirestore(app), "users", userUid, "links", linkID);
+        updateDoc(linkDocRef, { totalClicks: increment(1) });
         window.location.href = longUrl;
       } else {
         setLoading(false);
-      }
+      }      
     };
     fetchLinkDoc();
   }, []);
