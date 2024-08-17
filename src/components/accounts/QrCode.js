7f0bae98344bb6c06e752d189889d51cdfa1d013
Abeeb Abiola Adeniyi <abiolayoung229@gmail.com>
2024-08-17 23:07:27 +0300

    push

--- src/components/accounts/QrCode.js
+++ src/components/accounts/QrCode.js
@@ -9,7 +9,7 @@ import {
   CircularProgress,
 } from "@mui/material";
 import { ChromePicker } from "react-color";
-import { saveAs } from 'file-saver'
+import { saveAs } from "file-saver";
 import axios from "axios";
 
 // 068d7e00-58d1-11ef-8d5d-cff9db25dd8a - API-Key from QR-Tiger
@@ -83,25 +83,24 @@ const QrCode = () => {
   console.log(response);
   const handleSubmit = () => getQrCode();
   const downloadImage = () => {
-    saveAs(response, 'qrCode.png');
-  }
+    saveAs(response, "qrCode.png");
+  };
 
   return (
     <InputContext.Provider value={value}>
       <Box bgcolor="#56B7BA" height="100vh">
         <Navbar />
         <Box
+          width={{ xs: "80%", sm: "70%" }}
+          margin={{ xs: "20% auto", sm: "10% auto" }}
           bgcolor="#fff"
           style={{
-            width: "80%",
-            margin: "10% auto",
             borderRadius: "10px",
             boxShadow: "0px 10px 25px rgba(0, 0, 0, 0.1)",
           }}
-          mt={20}
         >
-          <Grid container spacing={2} mx={2}>
-            <Grid item style={{ width: "60%" }}>
+          <Grid container>
+            <Grid item width={{xs: '100%', sm: '60%'}} m={{xs: 2, sm: 3}}>
               <Typography>Enter Your URL</Typography>
               <TextField
                 id="outlined-basic"
@@ -111,42 +110,56 @@ const QrCode = () => {
                 value={inputValue.url}
                 onChange={handleChange}
               />
-              <Box mt={3}>
-                <Typography>Color</Typography>
+
+              <Box display='flex' alignItems={{sm: 'center', xs : 'left'}} justifyContent='space-between' flexDirection={{sm: 'row', xs: 'column'}}>
+                <Box my={1}>
+                  <Typography>Color</Typography>
                 <Box display="flex" alignItems="center">
-                  <Box
-                    mr={2}
-                    style={{
-                      width: "30px",
-                      height: "20px",
-                      cursor: "pointer",
-                      border: "5px solid #DFDBDC",
-                      backgroundColor: color,
-                    }}
-                    onClick={() => setDisplayColorPicker(!displayColorPicker)}
-                  ></Box>
-                  <span>{color}</span>
-                </Box>
-                {displayColorPicker && (
-                  <Box mt={2}>
-                    <ChromePicker
-                      color={color}
-                      onChange={(color) => setColor(color.hex)}
-                    />
+                    <Box
+                      mr={2}
+                      style={{
+                        width: "30px",
+                        height: "20px",
+                        cursor: "pointer",
+                        border: "5px solid #DFDBDC",
+                        backgroundColor: color,
+                      }}
+                      onClick={() => setDisplayColorPicker(!displayColorPicker)}
+                    ></Box>
+                    <span>{color}</span>
                   </Box>
-                )}
-              </Box>
-              <Box
-                marginTop="15%"
-                style={{ marginLeft: "65%" }}
-                onClick={handleSubmit}
-              >
-                <Button disableElevation variant="contained" color="primary" disabled={loading}>
-                  {loading ? <CircularProgress size={22} /> : "Generate QrCode"}
-                </Button>
-                {error ? <Typography>Sorry something went wrong</Typography> : ''}
+                  {displayColorPicker && (
+                    <Box mt={2}>
+                      <ChromePicker
+                        color={color}
+                        onChange={(color) => setColor(color.hex)}
+                      />
+                    </Box>
+                  )}
+                </Box>
+                
+                <Box mt={{sm: 5, xs: 2}}
+                  onClick={handleSubmit}
+                >
+                  <Button
+                    disableElevation
+                    variant="contained"
+                    color="primary"
+                    disabled={loading}
+                  >
+                    {loading ? <CircularProgress size={22} /> : "Generate Qr"}
+                  </Button>
+                  {error ? (
+                    <Typography>Sorry something went wrong</Typography>
+                  ) : (
+                    ""
+                  )}
+                </Box>
               </Box>
             </Grid>
+
+
+
             <Grid
               item
               display="flex"
@@ -154,11 +167,15 @@ const QrCode = () => {
               alignItems="center"
               margin="auto"
             >
-              {response ? (<img
-                src={response}
-                alt="qr-image"
-                style={{ width: "150px", height: "150px" }}
-              />) : (<Box>Your QR-code will show here...</Box>)}
+              {response ? (
+                <img
+                  src={response}
+                  alt="qr-image"
+                  style={{ width: "150px", height: "150px" }}
+                />
+              ) : (
+                <Box>Your QR-code will show here...</Box>
+              )}
               <Box my={2}>
                 <Button
                   disableElevation
